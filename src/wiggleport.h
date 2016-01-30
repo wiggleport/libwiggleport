4574adca5c0b05dfc4a3f09063d685082c6ae0b6
Micah Elizabeth Scott <micah@misc.name>
2016-01-29 16:27:03 -0800

    More API sketching
    Still trying to keep a formal JSON API out of the libwiggleport API, for both simplicity and multithreading reasons. Instead, trying to limit JSON use to things that are static and match lifetimes of their owning object.

--- src/wiggleport.h
+++ src/wiggleport.h
@@ -1,5 +1,9 @@
-// Copyright (c) 2016 Micah Elizabeth Scott
-// See attached LICENSE file for the MIT-style terms.
+/*
+ * Copyright (c) 2016 Micah Elizabeth Scott
+ *
+ * Wiggleport is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
 
 #ifndef _WIGGLEPORT_H_
 #define _WIGGLEPORT_H_
@@ -15,67 +19,86 @@ extern "C" {
 typedef struct wigl_context wigl_context;
 typedef struct wigl_enumerator wigl_enumerator;
 typedef struct wigl_model wigl_model;
+typedef struct wigl_tuple wigl_tuple;
 typedef struct wigl_action wigl_action;
+typedef struct wigl_schedule wigl_schedule;
 typedef struct wigl_stream wigl_stream;
 
 // Callbacks
 typedef void (wigl_enumerator_cb) (wigl_enumerator* en, void* cb_data);
+typedef void (wigl_tuple_cb) (wigl_tuple* tuple, void* cb_data);
+typedef void (wigl_schedule_cb) (wigl_schedule* sched, void* cb_data);
+typedef void (wigl_stream_cb) (wigl_stream* st, void* cb_data);
 
 // Context -- your connection to libwiggleport itself
 
-wigl_context* wigl_context_new(const char* options, char** err);
+wigl_context* wigl_context_new(const char* json_options, char** error);
 void wigl_context_unref(wigl_context* ctx);
 
-// Package management
+// Package management -- packages generate models based on the attached hardware
+//   (By default, libwiggleport loads a standard set of packages. Most programs
+//    won't need the package management functions.)
 
-bool wigl_package_load(wigl_context* ctx, const char* content, const char* files, char** error);
+bool wigl_package_load(wigl_context* ctx, const char* json_content, const char* json_files, char** error);
 bool wigl_package_load_file(wigl_context* ctx, const char* path, char** error);
-const char* wigl_package_inventory(wigl_context* ctx);
-const char* wigl_package_content(wigl_context* ctx, const char* name);
+const char* wigl_package_name(const wigl_context* ctx, int index);
+const char* wigl_package_json(const wigl_context* ctx, const char* name);
 
 // Enumerator -- find hardware models, and get hotplug notifications
 
-wigl_enumerator* wigl_enumerator_new(wigl_context* ctx, const char* options, char** error);
+wigl_enumerator* wigl_enumerator_new(wigl_context* ctx, const char* json_options, char** error);
 void wigl_enumerator_delete(wigl_enumerator* en);
 
-bool wigl_enumerator_dequeue(wigl_enumerator* en, wigl_model** add, wigl_model** remove);
+bool wigl_enumerator_next(wigl_enumerator* en, wigl_model** added, wigl_model** removed);
 void wigl_enumerator_notify(wigl_enumerator* en, wigl_enumerator_cb* cb, void* cb_data);
 
 // Model -- a data tree for interacting with hardware
 
-void wigl_model_unref(wigl_model* mod);
-const char* wigl_model_content(wigl_model* mod);
+void wigl_model_unref(wigl_model* model);
+const char* wigl_model_json(const wigl_model* model);
 
-void wigl_model_notify(wigl_model* mod, wigl_model_cb* cb, void* cb_data);
-// xxx: lifetime of this is bunk, probe object could be better?
+// Tuple -- an observed set of values from the model
 
-int64_t wigl_model_get_int(wigl_model* mod, const char* key, char** error);
-double wigl_model_get_number(wigl_model* mod, const char* key, char** error);
-const char* wigl_model_get_string(wigl_model* mod, const char* key, char** error);
-// xxx: obj lifetime of return is error prone; do we need strings? use cases?
+wigl_tuple* wigl_tuple_new(const wigl_model* model, const char* json_refs, char** error);
+void wigl_tuple_delete(wigl_tuple* tuple);
 
-// Action -- a change we want to make to the model
+bool wigl_tuple_next(wigl_tuple* tuple, char** json_change_detail);
+void wigl_tuple_notify(wigl_tuple* tuple, wigl_tuple_cb* cb, void* cb_data);
 
-wigl_action* wigl_action_new(wigl_model* mod, const char* dict, char** error);
+const char* wigl_tuple_json(const wigl_tuple* tuple);
+int64_t wigl_tuple_int(const wigl_tuple* tuple, int index, char** error);
+double wigl_tuple_number(const wigl_tuple* tuple, int index, char** error);
+const char* wigl_tuple_string(const wigl_tuple* tuple, int index, char** error);
+
+// Action -- a change that can be applied to the model
+
+wigl_action* wigl_action_new(const wigl_model* model, const char* json_dict, char** error);
 void wigl_action_unref(wigl_action* ac);
 
-bool wigl_action_set_int(wigl_action* ac, const char* key, int64_t value, char** error);
-bool wigl_action_set_number(wigl_action* ac, const char* key, int64_t value, char** error);
-bool wigl_action_set_string(wigl_action* ac, const char* key, const char* value, char** error);
+bool wigl_action_set_int(wigl_action* ac, const char* name, int64_t value, char** error);
+bool wigl_action_set_number(wigl_action* ac, const char* name, double value, char** error);
+bool wigl_action_set_string(wigl_action* ac, const char* name, const char* value, char** error);
 
-bool wigl_action_schedule(wigl_action* ac, const char* options, char** error);
-bool wigl_action_complet
-const char* wigl_action_detail()
+wigl_schedule* wigl_action_schedule(wigl_action* ac, const char* json_options, char** error);
+
+// Schedule -- things happening at a particular time across one or more streams
+
+void wigl_schedule_unref(wigl_schedule* sched);
+const char* wigl_schedule_json(const wigl_schedule* sched);
+
+bool wigl_schedule_has_finished(const wigl_schedule* sched);
+void wigl_schedule_notify(wigl_schedule* sched, wigl_schedule_cb* cb, void* cb_data);
 
 // Stream -- real-time I/O interface
 
-wigl_stream* wigl_stream_open(wigl_model* mod, const char* key, const char* mode, char** error);
+wigl_stream* wigl_stream_open(wigl_model* model, const char* name, const char* mode, char** error);
 void wigl_stream_close(wigl_stream* st);
+const char* wigl_stream_json(const wigl_stream* st);
 
-bool wigl_stream_has_fixed_rate(wigl_stream* st);
-double wigl_stream_get_rate_hz(wigl_stream* st);
-uint64_t wigl_stream_get_clock(wigl_stream* st);
+void wigl_stream_notify(wigl_stream* st, wigl_stream_cb* cb, void* cb_data);
 
+void wigl_stream_read();
+void wigl_stream_write();
 
 
 #ifdef __cplusplus
@@ -83,59 +106,3 @@ uint64_t wigl_stream_get_clock(wigl_stream* st);
 #endif
 
 #endif // _WIGGLEPORT_H_
-
-
-// Guidelines:
-//    - make it trivial to wrap in any language
-//    - JSON strings, minimal exposed memory management
-
-// handles? how?
-
-// What happens to superceded devices?
-//     - resources stay around, threads stop
-//     - can client choose to transparently port to new version?
-
-// (counted root reference, tree path)
-//   ^- client rep? hash? small int? OS handle?
-
-// Package management / Init
-
-//  -- Start up with/without standard packages
-//  -- Query installed packages
-//  -- Load/upgrade package
-
-// Class enumeration
-
-//  -- enumerate objects matching 'class'
-//     -- how is an object represented?
-//     -- what can I do to a class once it's found?
-
-/*
-
-per-user root service
-	"open class" broadcasts to all roots
-
-
-
-class client-side ID: invalidated on -reload-
-
-- open class
-   notify on exist, supercede (by new version or null)
-   JSON description of static content (exist, reload)
-   read-only SHM for dynamic content
-
-   - open stream
-      writeable SHM
-	  notify on read and/or write
-
-   - open expression
-	  notify on value, constraint error
-
-* event queues
-    - matching class
-    - expression
-  common insert/remove/monitor API?
-
-
-
-*/
